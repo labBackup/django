6a448f4b88b1fb4c22269ce641d5072e92253987
backupaccount <tempc122mail@gmail.com>
2026-03-14 16:10:37 +0530

    14th March

--- mayank/mysite/blog/views.py
+++ mayank/mysite/blog/views.py
@@ -1,10 +1,18 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
@@ -17,5 +25,3 @@ def post_detail(request, year, month, day, post):
         publish__day=day
         )
     return render(request, 'blog/post/detail.html', {'post': post})
-
-# Create your views here.
