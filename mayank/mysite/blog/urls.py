e8102d626624a781fc445b3c7306b27b2e4f725c
backupaccount <tempc122mail@gmail.com>
2026-03-21 16:04:11 +0530

    21th march

--- mayank/mysite/blog/urls.py
+++ mayank/mysite/blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # path('', views.post_list, name='post_list'), # old path before Class Based View (CBVs)
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/',views.post_share,name='post_share')
 ]
 
