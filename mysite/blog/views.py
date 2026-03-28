35a13cc495103bd00390756eab90349b8cfd3434
backupaccount <tempc122mail@gmail.com>
2026-03-28 14:21:03 +0530

    (chore) : update reformat

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -12,8 +13,12 @@ class PostListView(ListView):
     paginate_by = 2
     template_name = 'blog/post/list.html'
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     posts = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags_in=[tag])
     paginator = Paginator(posts, 2)
     page_number = request.GET.get('page', 1)
     try:
@@ -22,7 +27,7 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     except PageNotAnInteger:
         posts=paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts, 'tag':tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
